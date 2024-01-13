8b6bb626f8d0897ea54df458e657d4c70a15a76f
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2024-01-13 19:07:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ.?!,:;/=+-%@#€&*";
 
+let interval = null;
+
+for (var i = 0; i < document.querySelectorAll("h1").length; i++) {
+	document.querySelectorAll("h1")[i].onmouseover = (event) => {
+		let iteration = 0;
+
+		clearInterval(interval);
+
+		interval = setInterval(() => {
+			event.target.innerText = event.target.innerText
+				.split("")
+				.map((letter, index) => {
+					if (index < iteration) {
+						return event.target.dataset.value[index];
+					}
+
+					return letters[Math.floor(Math.random() * 42)];
+				})
+				.join("");
+
+			if (iteration >= event.target.dataset.value.length) {
+				clearInterval(interval);
+			}
+
+			iteration += 1 / 5;
+		}, 30);
+	};
+}
